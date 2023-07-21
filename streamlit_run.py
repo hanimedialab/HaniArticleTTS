9133a6dd86d282519a22ff82babbfde785b8a4a9
HaniMedialab <134350874+hanimedialab@users.noreply.github.com>
2023-07-21 22:10:18 +0900

    Update streamlit_run.py

--- streamlit_run.py
+++ streamlit_run.py
@@ -48,26 +48,26 @@ def make_filename(hani_url):
     return audio_filename, sub_filename, filehead
 
 # 스트리밍 오디오/자막 파일 생성
-# async def amain(text, voice, rate, volume, audio_filename, sub_filename):
-#     """Main function"""
-#     communicate = edge_tts.Communicate(text, voice, rate=rate, volume=volume)
-#     submaker = edge_tts.SubMaker()
-#     os.makedirs(os.path.dirname(audio_filename), exist_ok=True)
-#     with open(audio_filename, "wb") as file:
-#         async for chunk in communicate.stream():
-#             if chunk["type"] == "audio":
-#                 file.write(chunk["data"])
-#             elif chunk["type"] == "WordBoundary":
-#                 submaker.create_sub((chunk["offset"], chunk["duration"]), chunk["text"])
-#     os.makedirs(os.path.dirname(sub_filename), exist_ok=True)
-#     with open(sub_filename, "w", encoding="utf-8") as file:
-#         file.write(submaker.generate_subs())
-
-async def amain(text, voice, rate, volume, audio_filename):
+async def amain(text, voice, rate, volume, audio_filename, sub_filename):
     """Main function"""
     communicate = edge_tts.Communicate(text, voice, rate=rate, volume=volume)
+    submaker = edge_tts.SubMaker()
     os.makedirs(os.path.dirname(audio_filename), exist_ok=True)
-    await communicate.save(audio_filename)
+    with open(audio_filename, "wb") as file:
+        async for chunk in communicate.stream():
+            if chunk["type"] == "audio":
+                file.write(chunk["data"])
+            elif chunk["type"] == "WordBoundary":
+                submaker.create_sub((chunk["offset"], chunk["duration"]), chunk["text"])
+    os.makedirs(os.path.dirname(sub_filename), exist_ok=True)
+    with open(sub_filename, "w", encoding="utf-8") as file:
+        file.write(submaker.generate_subs())
+
+# async def amain(text, voice, rate, volume, audio_filename):
+#     """Main function"""
+#     communicate = edge_tts.Communicate(text, voice, rate=rate, volume=volume)
+#     os.makedirs(os.path.dirname(audio_filename), exist_ok=True)
+#     await communicate.save(audio_filename)
 
 # def make_mp3(text, voice, audio_filename, sub_filename):
 #     loop = asyncio.get_event_loop_policy().get_event_loop()
@@ -110,8 +110,8 @@ def app():
             text = get_article(hani_url)
             audio_filename, sub_filename, filehead = make_filename(hani_url)
             try:
-                asyncio.run(amain(text, voice, rate, volume, audio_filename))
-                # asyncio.run(amain(text, voice, rate, volume, audio_filename, sub_filename))
+                # asyncio.run(amain(text, voice, rate, volume, audio_filename))
+                asyncio.run(amain(text, voice, rate, volume, audio_filename, sub_filename))
                 with open(audio_filename, "rb") as f:
                     mp3_file = f.read()
                 st.audio(mp3_file, format='audio/mp3')
@@ -119,8 +119,8 @@ def app():
                 st.write("원본 기사: ", hani_url)
                 st.write("오디오 재생기 옆 '⋮' 버튼을 눌러 오디오 파일을 내려받을 수 있습니다.(확장자를 '.mp3'로 지정)")
 
-                # with open(sub_filename, "rb") as f:
-                #     st.download_button("자막 파일(VTT) 내려받기", f, file_name=filehead + '.vtt')
+                with open(sub_filename, "rb") as f:
+                    st.download_button("자막 파일(VTT) 내려받기", f, file_name=filehead + '.vtt')
             except Exception as e:
                 st.error(e)
 
